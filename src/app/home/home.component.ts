95f3c5b9c580677c41be39d8476d373f8c606e18
byllarreal <byllarreal@gmail.com>
2024-01-17 14:16:21 -0500

    Se reanderiza la información con Materil Angular mejorando character cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../service/api.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { NgClass } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,MatCardModule,MatButtonModule,NgClass],
   providers:[ApiService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{  
   data:any;
+  clase:string="";
   constructor(private servicio:ApiService){}  
   ngOnInit(): void {
     this.servicio.getData().subscribe(
